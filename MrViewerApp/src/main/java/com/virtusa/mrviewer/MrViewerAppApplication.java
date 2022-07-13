d5f76b330e73a60dff10a99305c1ead0b5644036
ReverB <ravivarmabandi@pop-os.localdomain>
2022-07-13 10:51:10 +0530

    Intial project

---
+++ MrViewerApp/src/main/java/com/virtusa/mrviewer/MrViewerAppApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.mrviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrViewerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrViewerAppApplication.class, args);
+	}
+
+}
